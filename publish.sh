49be7e3597f25ff9c2946e1914a918996f2542f8
Piasy <xz4215@gmail.com>
2016-08-22 21:01:48 +0800

    add index file

--- publish.sh
+++ publish.sh
@@ -4,5 +4,8 @@
 ./bin/md2resume html piasy_resume_zh.md dist/
 ./bin/md2resume pdf piasy_resume_zh.md dist/
 
+cp dist/piasy_resume_zh.html index.html
+cp dist/piasy_resume_zh.pdf resume.pdf
+
 git add . && git commit -m "$1"
 
